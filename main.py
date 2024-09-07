7cc231fccaac2f1076015dc09abeaa67db1431fb
Latheesh Mangeri <latheeshmangeri369@gmail.com>
2024-09-07 09:30:31 -0500

    qqewe

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ input_container = st.container()
 with input_container:
     query = st.text_area("Ask your AI Assistant", max_chars=10000, height=200, key="query_input")
 
-    if st.button("Send", use_container_width=True):
+    if st.button("Send"):
         if query:
             # Process files if any are uploaded
             if st.session_state.files_uploaded:
